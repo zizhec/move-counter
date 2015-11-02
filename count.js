4e0f83e5d560da88dd81f3b582961ef92a992fee
Eric <zizhec@163.com>
2015-11-03 03:19:52 +0800

    init

---
+++ count.js
@@ -0,0 +1,32 @@
+ angular.module('intervalExample', [])
+ .controller('timeCtrl', ['$scope', '$interval',
+ 	function($scope, $interval) {
+
+
+ 		$scope.currCounter = 0;
+ 		$scope.counterList = [];
+ 		$scope.isDisabled = true;
+ 		var timerMax = 2;
+ 		$scope.currTimer = timerMax;
+
+ 		$scope.start = function() {
+ 			 		$scope.isDisabled = false;
+ 			$interval(function() {
+ 				$scope.currTimer = $scope.currTimer - 1;
+
+ 				if($scope.currTimer == 0) {
+ 					var item = {
+ 						 time:new Date(),
+						 count:$scope.currCounter
+ 					}
+ 					$scope.counterList.push(item);
+ 					$scope.currTimer = timerMax;
+ 					$scope.currCounter = 0;
+ 					 $scope.isDisabled = true;
+ 				}
+ 			}, 1000, timerMax)
+ 		}
+
+
+ 	}
+ ]);
